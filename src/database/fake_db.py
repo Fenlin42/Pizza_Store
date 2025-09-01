8bb5c3577c173e2705baa015a46658cb35a14744
Fenlin <fenlin.chirakkal@fhnw.students.ch>
2025-09-01 23:43:47 +0200

    Setup

---
+++ src/database/fake_db.py
@@ -0,0 +1,9 @@
+from src.model.pizza import Pizza
+
+#Pizza_liste
+pizzas = []
+Pizza("Margherita", ["Käse", "Tomate"], 16.00)
+Pizza("Hawai", ["Schinken", "Ananas"], 18.00)
+Pizza("Diavola", ["scharfe_Salami", "Paprika"], 19.00)
+Pizza("Salami", ["Salami", "Käse"], 18.50)
+]
---
+++ src/model/pizza.py
@@ -0,0 +1,18 @@
+# Pizza- Klasse
+#Attribute:
+#name
+#toppings
+#price
+#METHODEN
+#price_with_discount
+
+
+
+
+#alle_pizzen_liste
+#pizza_eingegeben_belaegen
+#pizza_eingegeben_name
+#preis_mit_rabatt
+#
+
+
